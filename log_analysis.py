57f90ef39ec23fb1805133a138988d5bc657ae52
Letitia Kernschmidt <letitiakernschmidt@gmail.com>
2017-11-09 19:17:56 +0100

    Format results text

--- log_analysis.py
+++ log_analysis.py
@@ -16,7 +16,7 @@ result_file.write("\n The three most popular articles of all time: \n")
 cur.execute("SELECT * FROM article_views LIMIT 3;")
 results1 = cur.fetchall()
 for item in results1:
-    result_file.write("* %s views \n" % " - ".join(map(str, item)))
+    result_file.write("* \"%s views \n" % "\" -- ".join(map(str, item)))
 
 # Who are the most popular article authors of all time?
 result_file.write("\n The most popular authors of all time: \n")
@@ -25,18 +25,18 @@ cur.execute("""SELECT name, sum(views) as views FROM article_views, name_title
             ORDER BY views DESC;""")
 results2 = cur.fetchall()
 for item in results2:
-    result_file.write("* %s views \n" % " - ".join(map(str, item)))
+    result_file.write("* %s views \n" % " -- ".join(map(str, item)))
 
 # On which days did more than 1% of requests lead to errors?
 result_file.write("\n Days with more than 1% error rate: \n")
 cur.execute("""SELECT day, percent
-            FROM
-            (SELECT day, ROUND(CAST(errors/CAST(ok AS FLOAT)*100 AS NUMERIC),2)
+            FROM (SELECT day::date,
+            ROUND(CAST(errors/CAST(ok AS FLOAT)*100 AS NUMERIC),2)
             AS percent FROM status) AS sub
             WHERE percent > 1;""")
 results3 = cur.fetchall()
 for item in results3:
-    result_file.write("* %s%% errors \n" % " - ".join(map(str, item)))
+    result_file.write("* %s%% errors \n" % " -- ".join(map(str, item)))
 
 # Close connection to database
 cur.close()
