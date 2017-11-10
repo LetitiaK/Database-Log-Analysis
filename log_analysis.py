8c2bcd3b608c74be5f3381802c033c9a2dfaa85d
Letitia Kernschmidt <letitiakernschmidt@gmail.com>
2017-11-10 17:46:36 +0100

    Reformat code to contain functions

--- log_analysis.py
+++ log_analysis.py
@@ -8,40 +8,50 @@
 
 import psycopg2
 
+
 # Connect to database
-conn = psycopg2.connect("dbname=news")
-cur = conn.cursor()
+def connect(database_name="news"):
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        cur = db.cursor()
+        return db, cur
+    except BaseException:
+        print("Unable to connect to database")
+
+
+# Perform SQL query
+def sql_query(headline, query, result_file, category):
+    db, cur = connect()
+    result_file.write(headline)
+    cur.execute(query)
+    results = cur.fetchall()
+    write_results(results, category)
+    cur.close()
+    db.close()
+
+
+# Format output correctly depending on query
+def write_results(results, category):
+    for item in results:
+        if category == "article":
+            result_file.write("* \"%s views \n" % "\" -- ".
+                              join(map(str, item)))
+        elif category == "author":
+            result_file.write("* %s views \n" % " -- ".join(map(str, item)))
+        elif category == "percent":
+            result_file.write("* %s%% errors \n" % " -- ".join(map(str, item)))
+
 
 # Create output file
 result_file = open("results.txt", "w")
-
-# What are the most popular three articles of all time?
-result_file.write("\n The three most popular articles of all time: \n")
-cur.execute("SELECT * FROM article_views LIMIT 3;")
-results1 = cur.fetchall()
-for item in results1:
-    result_file.write("* \"%s views \n" % "\" -- ".join(map(str, item)))
-
-# Who are the most popular article authors of all time?
-result_file.write("\n The most popular authors of all time: \n")
-cur.execute("""SELECT name, sum(views) as views FROM article_views, name_title
-            WHERE article_views.title=name_title.title GROUP BY name
-            ORDER BY views DESC;""")
-results2 = cur.fetchall()
-for item in results2:
-    result_file.write("* %s views \n" % " -- ".join(map(str, item)))
-
-# On which days did more than 1% of requests lead to errors?
-result_file.write("\n Days with more than 1% error rate: \n")
-cur.execute("""SELECT day, percent
-            FROM (SELECT day,
-            ROUND(CAST(errors/CAST(ok + errors AS FLOAT)*100 AS NUMERIC),2)
-            AS percent FROM status) AS sub
-            WHERE percent > 1;""")
-results3 = cur.fetchall()
-for item in results3:
-    result_file.write("* %s%% errors \n" % " -- ".join(map(str, item)))
-
-# Close connection to database
-cur.close()
-conn.close()
+sql_query("\n The three most popular articles of all time: \n",
+          "SELECT * FROM article_views LIMIT 3;", result_file, "article")
+sql_query("\n The most popular authors of all time: \n",
+          """SELECT name, sum(views) as views FROM article_views, name_title
+          WHERE article_views.title=name_title.title GROUP BY name
+          ORDER BY views DESC;""", result_file, "author")
+sql_query("\n Days with more than 1% error rate: \n",
+    	     """SELECT day, percent FROM (SELECT day,
+             ROUND(CAST(errors/CAST(ok + errors AS FLOAT)*100 AS NUMERIC),2)
+             AS percent FROM status) AS sub
+             WHERE percent > 1;""", result_file, "percent")
