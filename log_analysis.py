2781ba1e351e231c2284366ef1328dda05ccee13
Letitia Kernschmidt <letitiakernschmidt@gmail.com>
2017-11-11 18:30:52 +0100

    Restructure code to functions

--- log_analysis.py
+++ log_analysis.py
@@ -19,39 +19,68 @@ def connect(database_name="news"):
         print("Unable to connect to database")
 
 
-# Perform SQL query
-def sql_query(headline, query, result_file, category):
+# Perform SQL query for top 3 article_views
+def getTopArticles(file_name):
     db, cur = connect()
-    result_file.write(headline)
+    query = "SELECT * FROM article_views LIMIT 3;"
     cur.execute(query)
-    results = cur.fetchall()
-    write_results(results, category)
+    result = cur.fetchall()
+    writeResults(file_name, result, "article")
+    cur.close()
+    db.close()
+
+
+# Perform SQL query for top 3 authors
+def getTopAuthors(file_name):
+    db, cur = connect()
+    query = """SELECT name, sum(views) as views FROM article_views, name_title
+    WHERE article_views.title=name_title.title GROUP BY name
+    ORDER BY views DESC;"""
+    cur.execute(query)
+    result = cur.fetchall()
+    writeResults(file_name, result, "author")
+    cur.close()
+    db.close()
+
+
+# Perform SQL query for dates with error rate > 1%
+def getErrorDates(file_name):
+    db, cur = connect()
+    query = """SELECT day, percent FROM (SELECT day,
+    ROUND(CAST(errors/CAST(ok + errors AS FLOAT)*100 AS NUMERIC),2)
+    AS percent FROM status) AS sub
+    WHERE percent > 1;"""
+    cur.execute(query)
+    result = cur.fetchall()
+    writeResults(file_name, result, "error")
     cur.close()
     db.close()
 
 
 # Format output correctly depending on query
-def write_results(results, category):
+def writeResults(result_file, results, category):
+    # result_file = open(file_name, "w")
+    if category == "article":
+        result_file.write("\n The three most popular articles of all time: \n")
+    elif category == "author":
+        result_file.write("\n The most popular authors of all time: \n")
+    elif category == "error":
+        result_file.write("\n Days with more than 1% error rate: \n")
     for item in results:
         if category == "article":
-            result_file.write("* \"%s views \n" % "\" -- ".
-                              join(map(str, item)))
+            result_file.write("* \"%s views \n" % "\" -- ".join(map(str, item)))  # NOQA
         elif category == "author":
             result_file.write("* %s views \n" % " -- ".join(map(str, item)))
-        elif category == "percent":
+        elif category == "error":
             result_file.write("* %s%% errors \n" % " -- ".join(map(str, item)))
 
 
 # Create output file
-result_file = open("results.txt", "w")
-sql_query("\n The three most popular articles of all time: \n",
-          "SELECT * FROM article_views LIMIT 3;", result_file, "article")
-sql_query("\n The most popular authors of all time: \n",
-          """SELECT name, sum(views) as views FROM article_views, name_title
-          WHERE article_views.title=name_title.title GROUP BY name
-          ORDER BY views DESC;""", result_file, "author")
-sql_query("\n Days with more than 1% error rate: \n",
-    	     """SELECT day, percent FROM (SELECT day,
-             ROUND(CAST(errors/CAST(ok + errors AS FLOAT)*100 AS NUMERIC),2)
-             AS percent FROM status) AS sub
-             WHERE percent > 1;""", result_file, "percent")
+def writeReport(file_name):
+    with open(file_name, "w") as result_file:
+        getTopArticles(result_file)
+        getTopAuthors(result_file)
+        getErrorDates(result_file)
+
+
+writeReport("results.txt")
